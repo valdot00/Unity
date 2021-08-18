85d67bd341b85dc12d0f47d56326ed9caf9ce27f
Val <valdo_jopa@hotmail.com>
2021-08-18 11:50:59 -0500

    testing

---
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int carDooors = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(3+5);
+
+        Debug.Log(carDooors-3);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
