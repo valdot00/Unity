aa26e5e5a7d2068b1a404686d78a6e2a380ade4e
Val <valdo_jopa@hotmail.com>
2021-08-19 17:51:51 -0500

    testing Update()

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public int carDooors = 4;
+    //public int carDooors = 4;
+
+    public int x = 2;
+    public int y = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(3+5);
+        //Debug.Log(3+5);
 
-        Debug.Log(carDooors-3);
-        
+        //Debug.Log(carDooors-3);
+
+        Sumalosdosnumeros();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //Sumalosdosnumeros();
+    }
+    void Sumalosdosnumeros(){
+
+        Debug.Log(x+y);
+
     }
 }
