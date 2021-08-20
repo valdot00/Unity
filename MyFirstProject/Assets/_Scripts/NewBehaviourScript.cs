68b5ef2b167944af9e9a224e6137cb544507356c
Val <valdo_jopa@hotmail.com>
2021-08-20 11:03:32 -0500

    testing script

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -8,6 +8,20 @@ public class NewBehaviourScript : MonoBehaviour
 
     public int x = 2;
     public int y = 3;
+    public string name = "jorge oswaldo pelayo arellano" ;
+    private int age = 29;
+
+    int level;
+    string haircolor = "red";
+    int money = 7;
+
+    //double p = 20.11111;
+    //float p2 = 20.13164;
+
+    private int health = 100;
+
+     public bool bossDefeated = false;
+     public bool hasTheKey = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +30,13 @@ public class NewBehaviourScript : MonoBehaviour
 
         //Debug.Log(carDooors-3);
 
-        Sumalosdosnumeros();
+        //Sumalosdosnumeros();
+
+        Debug.Log("hoy es vierne "+name);
+
+        Debug.LogFormat("el valor de {0} mas el valor de {1} resula {2}",x,y,x+y);
+
+        Debug.LogFormat("el valor de "+x+" mas el valor de "+y+" resula ",x+y);
     }
     // Update is called once per frame
     void Update()
@@ -27,5 +47,16 @@ public class NewBehaviourScript : MonoBehaviour
 
         Debug.Log(x+y);
 
+    }
+
+    public void Mimetodo(){
+
+    }
+    public void Mimetoido2(){
+
+    }
+
+    public void MakeDamage(){
+        health-=10;
     }
 }
