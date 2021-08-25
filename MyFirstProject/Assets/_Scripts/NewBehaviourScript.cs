8142ae042a42ac87c54c28942be8e2e32ecd8c75
Val <valdo_jopa@hotmail.com>
2021-08-25 12:07:54 -0500

    val testing  GenerateCharacter

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -2,84 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class NewBehaviourScript : MonoBehaviour
 {
-    //public int carDooors = 4;
 
-    public int x = 2;
-    public int y = 3;
-    public string name = "jorge oswaldo pelayo arellano" ;
-    private int age = 29;
-
-    int level;
-    string haircolor = "red";
-    int money = 7;
-
-    float temperature = 25;
-    int piNumber = (int)3.14159265;
-
-
-    //double p = 20.11111;
-    //float p2 = 20.13164;
-
-    private int health = 100;
-
-     public bool bossDefeated = false;
-     public bool hasTheKey = true;
-
-    // Start is called before the first frame update
+    public string playerName = "jon snow";
+    public int levelCharecter = 1;
+   
     void Start()
     {
-        //Debug.Log(3+5);
-
-        //Debug.Log(carDooors-3);
-
-        //Sumalosdosnumeros();
-
-        //Debug.Log("hoy es vierne "+name);
-
-        //Debug.LogFormat("el valor de {0} mas el valor de {1} resula {2}",x,y,x+y);
-
-        //Debug.LogFormat("el valor de "+x+" mas el valor de "+y+" resula ",x+y);
-
-        string sentence = $"el valor de x es {x}";
-        Debug.Log(sentence);
-
-        var currentage = false;
-       // float suma = (float)currentage+price;
-
-
-
+       Debug.Log("vamos a llamar a GENERATE CHARACTER");
+       GenerateCharacter(playerName,levelCharecter);
+       Debug.Log("hemos llamado a generate character");
     }
-    // Update is called once per frame
+   
     void Update()
     {
-        //Sumalosdosnumeros();
-    }
-    void Sumalosdosnumeros(){
-
-        Debug.Log(x+y);
-
+        
     }
 
-    public void Mimetodo(){
+    public void GenerateCharacter(string name,int level){
 
-    }
-    public void Mimetoido2(){
-
-
-    }
-
-    public void MakeDamage(){
-        health-=10;
-    }
-
-    void CreateCharacter(){
-        string characterName = "PEPE";
-        Debug.Log("el nombre del personaje es: "+characterName);
-
-        if(true){
-            bool success = true;
-        }
+        Debug.LogFormat("personaje: {0} de nivel {1}",name,level );
     }
 }
