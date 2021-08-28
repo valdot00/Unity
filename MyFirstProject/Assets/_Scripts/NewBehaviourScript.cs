07036b3c52596b9601cf5184fab2f9ecede55924
Val <valdo_jopa@hotmail.com>
2021-08-28 11:56:11 -0500

    testing Key

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -6,13 +6,19 @@ public class NewBehaviourScript : MonoBehaviour
 {
     void Start()
     {
-       
 
+        public bool hasTheKeyToDungeon = true;
+       
     }
     
     void Update()
     {
+        if(hasTheKeyToDungeon){
+            Debug.Log("tienes la llave, en horabuena");
+        }else{
+            Debug.Log("No puedes pasaaaaaar");
+        }
         
     }
-    
+
 }
