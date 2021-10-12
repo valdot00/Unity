a4c7b877f882bee41bd8c57610232b230b3a3aab
Val <valdo_jopa@hotmail.com>
2021-10-12 11:29:14 -0500

    first Score

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    int[] ranking = new int[]{4,7,9};
     void Start()
     {
         //array
 
         //int[] ranking = new int[3]; //[0,0,0];
 
-        int[] ranking = new int[]{4,7,9};
+       int firstScore = ranking[0]
+
+       ranking[2]=125;
+
+       //int someNumber = ranking[8];
+
+       int arrayLength = ranking.Length; //3
+
+       int pos = 8;
+
+       if(pos >=0 && pos < ranking.Length){
+           int someNumber = ranking[pos];
+       }
 
     }
     
