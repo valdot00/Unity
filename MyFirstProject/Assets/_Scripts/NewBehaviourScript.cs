f989aa80cc4d7e91ddf45b8f81c729265806ecd6
Val <valdo_jopa@hotmail.com>
2021-11-12 17:15:18 -0600

    list NPS
    testing list NPS in the game

--- MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
+++ MyFirstProject/Assets/_Scripts/NewBehaviourScript.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     int[] ranking = new int[]{4,7,9};
+    //LIST
+     List<string> partyMembers = new List<string>(){
+           "ARAGON","SNAKE","SOLID SNAKE","VENOM SNAKE","LIQUIT SNAKE"
+       };
+
+
     void Start()
     {
         //array
 
         //int[] ranking = new int[3]; //[0,0,0];
 
-       int firstScore = ranking[0]
+       int firstScore = ranking[0];
 
        ranking[2]=125;
 
@@ -24,6 +30,19 @@ public class NewBehaviourScript : MonoBehaviour
        if(pos >=0 && pos < ranking.Length){
            int someNumber = ranking[pos];
        }
+
+       string firstMember = partyMembers[0];
+       Debug.LogFormat("nuestra comunidad tiene {0} miembros",partyMembers.Count);
+
+       partyMembers.Add("dontomas");
+       partyMembers.Add("don chui");
+
+       partyMembers.Insert(2,"sam");
+
+        Debug.LogFormat("nuestra comunidad tiene {0} miembros",partyMembers.Count);
+
+        partyMembers.Remove("don chui");
+        partyMembers.RemoveAt(3);
 
     }
     
